299ff68a48194a2218ef669ee7411cb51cf3ebd9
jheckel <jalen.heckel@icloud.com>
2021-11-08 12:38:43 -0800

    finished questions, working on calling function to initial application

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
-const util = require("util");
 const generateMarkdown = require("utils/generateMarkdown.js");
 
 
@@ -16,22 +15,65 @@ const questions = [
         name: "username",
     },
     {
-        
-    }
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What is the name of your project?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "How would you describe the nature of your project?",
+        name: "description"
+    },
+    {
+        type: "list",
+        message: "Are you using any unique licenses for this project?",
+        default: "(Use Arrow Keys to Navigate)",
+        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BDS 3", "NONE"],
+        name: "license"
+    },
+    {
+        type: "input",
+        message: "What command should we run to install your dependencies?",
+        default: "npm i",
+        name: "install"
+    },
+    {
+        type: "input",
+        message: "How are we running tests fot this project?",
+        default: "npm test",
+        name: "test"
+    },
+    {
+        type: "input",
+        message: "Is there anything the user needs to know about using this repo?",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "Does the user need to know anytning about contributing to this repo?",
+        name: "contribution"
+    },
 ];
 
 
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
 
 
 
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt
+}
 
 
 
