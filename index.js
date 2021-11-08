f25934b1719d1b2a6b1640128a7a78775988fa1f
jheckel <jalen.heckel@icloud.com>
2021-11-08 12:25:55 -0800

    declared variables for index.js dependencies, entering questions.

---
+++ index.js
@@ -0,0 +1,42 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const generateMarkdown = require("utils/generateMarkdown.js");
+
+
+
+
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: "input",
+        message: "What's your GitHub username?",
+        name: "username",
+    },
+    {
+        
+    }
+];
+
+
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+
+
+
+
+// TODO: Create a function to initialize app
+function init() {}
+
+
+
+
+
+
+// Function call to initialize app
+init();
